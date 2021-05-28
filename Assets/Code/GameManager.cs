054dbe0198490baff2b1855966c9f04471fc752c
parsiash <parsia.shahini@gmail.com>
2021-05-28 21:23:10 +0430

    Basic GameManager added

---
+++ Assets/Code/GameManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using MiniRPG.Common;
+using UnityEngine;
+
+namespace MiniRPG
+{
+    public class GameManager : SingletonBehaviour<GameManager>
+    {
+    }
+}

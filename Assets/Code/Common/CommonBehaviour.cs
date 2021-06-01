4a16549a943023869bf474afd61f62f9fe61f623
parsiash <parsia.shahini@gmail.com>
2021-06-01 15:36:28 +0430

    Caching and retrieving components in CommonBehaviour implemented

--- Assets/Code/Common/CommonBehaviour.cs
+++ Assets/Code/Common/CommonBehaviour.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace MiniRPG.Common
@@ -8,8 +10,74 @@ namespace MiniRPG.Common
     /// </summary>
     public class CommonBehaviour : MonoBehaviour
     {
-        protected ILogger logger => DefaultLogger.Instance;
+         private Dictionary<string, Component> _cachedComoponents;
+        private Dictionary<string, Component> cachedComoponents
+        {
+            get
+            {
+                _cachedComoponents = _cachedComoponents ?? new Dictionary<string, Component>();
+                return _cachedComoponents;
+            }
+        }
+
+        private Dictionary<string, Component> _cachedComoponentsInChildren;
+        private Dictionary<string, Component> cachedComoponentsInChildren
+        {
+            get
+            {
+                _cachedComoponentsInChildren = _cachedComoponentsInChildren ?? new Dictionary<string, Component>();
+                return _cachedComoponentsInChildren;
+            }
+        }
         
+        protected ILogger logger => DefaultLogger.Instance;
+
+          private string GetComponentTypeName<T>() where T : Component
+        {
+            return GetComponentTypeName(typeof(T));
+        }
+
+        private string GetComponentTypeName(Type type)
+        {
+            return type.Name;
+        }
+
+        public T RetrieveCachedComponent<T>() where T : Component
+        {
+            var name = GetComponentTypeName<T>();
+
+            if(cachedComoponents.TryGetValue(name, out var component))
+            {
+                return component as T;
+            }
+
+            component = GetComponent<T>();
+            if(component)
+            {
+                cachedComoponents[name] = component;
+            }
+
+            return component as T;
+        }        
+
+        public T RetrieveCachedComponentInChildren<T>(bool includeInActive = true) where T : Component
+        {
+            var name = GetComponentTypeName<T>();
+
+            if(cachedComoponentsInChildren.TryGetValue(name, out var component))
+            {
+                return component as T;
+            }
+
+            component = GetComponentInChildren<T>(includeInActive);
+            if(component)
+            {
+                cachedComoponentsInChildren[name] = component;
+            }
+
+            return component as T;
+        }
+
         public void SetActive(bool active)
         {
             gameObject.SetActive(active);
